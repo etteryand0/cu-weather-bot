45ca58c54b46c3b149907721c5d7aef2c4723134
Sergey Sergin <etteryand@gmail.com>
2024-12-20 23:02:40 +0300

    feat: dialogue state msg handler filter

---
+++ weather_bot/filters.py
@@ -0,0 +1,21 @@
+from typing import Union
+
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+
+from weather_bot.context import Context
+
+
+class DialogueStateFilter(BaseFilter):
+    def __init__(self, state: Union[str, list[str]]):
+        self.state = state
+
+    async def __call__(self, message: Message, ctx: Context) -> bool:
+        dialogue = ctx.get_dialogue(message.from_user.id)
+        if dialogue is None:
+            return False
+
+        if isinstance(self.state, list):
+            return dialogue.state in self.state
+
+        return dialogue.state == self.state
